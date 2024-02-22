9ec778f24a5293be9092b812d7c28edd5bfbb8c7
Felix Lee <felix.yy.lee@gmail.com>
2024-02-22 03:31:18 -0500

    HMTY-34 Modify PDF generation to solely client side

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import { SERVER } from "./constants.js";
 import { userRouter } from "./userService.js";
 import { connectToMongo } from "./database.js";
-import { customizedPDF } from "./customizedPDF.js";
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.listen(SERVER.PORT, () => {
 // Cors and register all services
 app.use(cors());
 app.use("/user", userRouter); // User service - handles registration, login, etc
-app.use("/customizedPDF", customizedPDF); // Create and download customized PDF
 
 // Basic test endpoint
 app.get("/ping", express.json(), async (req, res) => {
--- client/src/CustomizedPDF.js
+++ client/src/CustomizedPDF.js
@@ -1,39 +1,32 @@
-const PORT = 4000;
-const path = 'http://localhost:' + PORT; // Need to change this after we know corresponding path
-const provider_code = 'BB-CC234'; // This should be change after we know unique code
+import jsPDF from 'jspdf';
 
+const prescriber_code = "CC-DD234"; // This should be change after we know unique code
 
 function CustomizedPDF() {
 
   const handleDownload = async () => {
-    try {
-      // Create Customized PDF
-      await fetch(path + "/customizedPDF/createcustomized", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ rewrite: provider_code })
-      });
+    // Create Customized PDF
+    const doc = new jsPDF();
+    const margin = 10;
+    doc.setFontSize(14);
+    doc.text("Name _______________________________________", margin, 40);
 
-      // Download Customized PDF
-      const response = await fetch(`${path}/customizedPDF/downloadCustomized`);
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', "PaRx-" + provider_code + '.pdf');
-      document.body.appendChild(link);
-      link.click();
+    doc.text("Date ________________________________________", margin, 60);
 
-      URL.revokeObjectURL(url);
+    doc.text("My Outdoor Activity Plan:", margin, 80);
+    
+    doc.text("________________________________________", margin, 250);
 
-      // Remove Customized PDF 
-      await fetch(`${path}/customizedPDF/removeCustomized`, {method: "DELETE"});
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred while downloading the file.');
-    }
+    doc.setFontSize(10);
+    doc.text("Health Professional’s Signature", margin, 255);
+
+    doc.setFontSize(14);
+    doc.text("Prescription #:  " + prescriber_code + "  –  _________________  –  _________________", margin, 270);
+
+    doc.setFontSize(10);
+    doc.text("(YYMMDD)", margin+80, 275);
+    doc.text("(Patient’s Initials)", margin+130, 275);
+    doc.save("PaRx-" + prescriber_code + ".pdf");
   };
 
   return (
