b5b9331dd49fe4f87b263a0cd9de3e019df16812
Chun Kang Lu <chunkanglu@gmail.com>
2024-02-22 20:46:01 +0000

    HMTY-33 add onto verify prescriber scaffold

--- backend/functional/verification/verify.js
+++ backend/functional/verification/verify.js
@@ -7,7 +7,7 @@ import scraperNS from "./scrapers/scraperNS"
 import scraperON from "./scrapers/scraperON"
 import scraperPE from "./scrapers/scraperPE"
 import scraperQC from "./scrapers/scraperQC"
-import ScraperSK from "./scrapers/scraperSK"
+import scraperSK from "./scrapers/scraperSK"
 
 const MOCK_INPUT_DATA = [
     {
@@ -26,20 +26,59 @@ const MOCK_INPUT_DATA = [
     }
 ]
 
+/**
+ * Returns corresponding scraper to use for a given prescriber.
+ * If no valid scraper found, an error is thrown.
+ * 
+ * @param {object} prescriber prescriber data object
+ * @returns {BaseScraper}
+ */
+function getScraper(prescriber) {
+    switch (prescriber.licenceCollege) {
+        case "College of Physicians and Surgeons of Alberta":
+            return scraperAB;
+        case "College of Physicians and Surgeons of British Columbia":
+            return scraperBC;
+        case "College of Physicians and Surgeons of Manitoba":
+            return scraperMB;
+        case "College of Physicians and Surgeons of New Brunswick":
+            return scraperNB;
+        case "College of Physicians and Surgeons of Newfoundland and Labrador":
+            return scraperNL;
+        case "College of Physicians and Surgeons of Nova Scotia":
+            return scraperNS;
+        case "College of Physicians and Surgeons of Ontario":
+            return scraperON;
+        case "College of Physicians & Surgeons of Prince Edward Island":
+            return scraperPE;
+        case "Collège des médecins du Québec":
+            return scraperQC;
+        case "College of Physicians and Surgeons of Saskatchewan":
+            return scraperSK;
+        default:
+            throw new Error("Unrecognized licensing college");
+    }
+}
+
 
 export function verifyPrescribers(inputData) {
     let unverified = [];
-
-    // Loop through data
-
-    // Find and call corresponding scraper
-
-    // If verified
-
-        // Update their status in DB
+    let verified = []; // probably unnecessary
     
-        // Async call to other function to generate code & sign up link 
+    for (prescriber of inputData) {
+        let scraper = getScraper(prescriber);
+        let isVerified = scraper.getStatus(prescriber);
 
-    // Else add to unverified with reason (and possibly other error handling)
+        // If verified
+
+            // Update their status in DB
+        
+            // Async call to other function to generate code & sign up link 
+
+        // Else add to unverified with reason (and possibly other error handling)
+
+    }
+
+    // Some actions & returns for unverified (and maybe verified?)
 
 }
