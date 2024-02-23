ee750d79a21485ff649ab775c8f13afb9b224497
Chun Kang Lu <chunkanglu@gmail.com>
2024-02-23 22:27:06 +0000

    HMTY-33 refactor scrapers

--- backend/functional/verification/scrapers/scraperAB.js
+++ backend/functional/verification/scrapers/scraperAB.js
@@ -1,26 +1,26 @@
-import parseTable from "./utils"
-
-export const scraperAB = ScraperAB();
+import parseTable from "./utils.js"
 
 class ScraperAB extends BaseScraper {
-    constructor() {
-        this.scrapeUrl = "https://search.cpsa.ca/"
-    }
+    static scrapeUrl = "https://search.cpsa.ca/";
+    static firstNameLocator = "#MainContent_physicianSearchView_txtFirstName";
+    static lastNameLocator = "#MainContent_physicianSearchView_txtLastName";
+    static searchButtonLocator = "#MainContent_physicianSearchView_btnSearch";
+    static resultTableLocator = "#MainContent_physicianSearchView_gvResults > tbody";
 
     async getStatus(prescriber, driver) {
         // Uses last name, first name
 
-        await driver.goto("https://search.cpsa.ca/", {waitUntil: 'networkidle2'});
-        await driver.type("#MainContent_physicianSearchView_txtFirstName", prescriber.firstName);
-        await driver.type("#MainContent_physicianSearchView_txtLastName", prescriber.lastName);
+        await driver.goto(ScraperAB.scrapeUrl, {waitUntil: 'networkidle2'});
+        await driver.type(ScraperAB.firstNameLocator, prescriber.firstName);
+        await driver.type(ScraperAB.lastNameLocator, prescriber.lastName);
 
-        await driver.click("#MainContent_physicianSearchView_btnSearch")
+        await driver.click(ScraperAB.searchButtonLocator)
         // This waits for the process after button click to fully load
         await driver.waitForNetworkIdle();
         
         // Parse result table as 2D array
         // TODO: try to optimize to use only columns we need to check
-        const table = await parseTable("#MainContent_physicianSearchView_gvResults > tbody", driver);
+        const table = await parseTable(ScraperAB.resultTableLocator, driver);
 
         for (let row in table) {
             // Name in first column
