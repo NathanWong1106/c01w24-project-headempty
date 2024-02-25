117b13cddeab34a8a0907063792854d89db9c857
Chun Kang Lu <chunkanglu@gmail.com>
2024-02-25 15:19:39 -0500

    HMTY-33 add try-catch & temp logging for BC

--- backend/functional/verification/scrapers/scraperBC.js
+++ backend/functional/verification/scrapers/scraperBC.js
@@ -31,60 +31,69 @@ export class ScraperBC extends BaseScraper {
         // Uses last name, first name
         // Need to first click on advanced search
 
-        const firstNameRegex = new RegExp("\\b" + prescriber.firstName + "\\b");
-        const lastNameRegex = new RegExp("\\b" + prescriber.lastName + "\\b");
+        try {
+            const firstNameRegex = new RegExp("\\b" + prescriber.firstName + "\\b");
+            const lastNameRegex = new RegExp("\\b" + prescriber.lastName + "\\b");
 
-        await driver.goto(ScraperBC.scrapeUrl, {waitUntil: 'networkidle2'});
-        await driver.click(ScraperBC.advancedSearchLocator);
-        await driver.type(ScraperBC.firstNameLocator, prescriber.firstName);
-        await driver.type(ScraperBC.lastNameLocator, prescriber.lastName);
-        await driver.click(ScraperBC.searchButtonLocator);
-        await driver.waitForNavigation();
+            await driver.goto(ScraperBC.scrapeUrl, {waitUntil: 'networkidle2'});
+            await driver.click(ScraperBC.advancedSearchLocator);
+            await driver.type(ScraperBC.firstNameLocator, prescriber.firstName);
+            await driver.type(ScraperBC.lastNameLocator, prescriber.lastName);
+            await driver.click(ScraperBC.searchButtonLocator);
+            await driver.waitForNavigation();
 
-        // Results are inside the div with class results-main-wrapper
-        // Each result has class result-item
-        // Name located in first div of a result, in an 'a' in an h5 3 layers down
-        // Status is located in the second div of a result
-        const resultWrapper = await driver.$(ScraperBC.resultWrapperLocator);
-        const results = await resultWrapper.$$(ScraperBC.resultsLocator);
+            // Results are inside the div with class results-main-wrapper
+            // Each result has class result-item
+            // Name located in first div of a result, in an 'a' in an h5 3 layers down
+            // Status is located in the second div of a result
+            const resultWrapper = await driver.$(ScraperBC.resultWrapperLocator);
+            const results = await resultWrapper.$$(ScraperBC.resultsLocator);
 
-        // Check if any matches
-        const numMatches = results.length;
-        if (numMatches === 0) {
-            // TODO: change to some other logging?
-            console.warn(`No matches for: ${prescriber.firstName} ${prescriber.lastName}`);
-            return null;
-        }
-        else if (numMatches >= 2) {
-            // TODO: change to some other logging?
-            console.warn(`More than 1 match for prescriber: ${prescriber.firstName} ${prescriber.lastName}. Selecting first instance.`);
-        }
+            // Check if any matches
+            const numMatches = results.length;
+            if (numMatches === 0) {
+                // TODO: change to some other logging?
+                console.warn(`No matches for: ${prescriber.firstName} ${prescriber.lastName}`);
+                return null;
+            }
+            else if (numMatches >= 2) {
+                // TODO: change to some other logging?
+                console.warn(
+                    `More than 1 match for prescriber: ${prescriber.firstName} ${prescriber.lastName}.
+                    Selecting first instance if multiple fully match.`
+                );
+            }
 
-        for (const result of results) {
-            const name = await result.evaluate(ele => {
-                const nameElement = ele.getElementsByTagName("a")[0];
-                // Need to remove trailing arrow_forward from a connected span element
-                return nameElement.textContent.trim().replace(/\s*arrow_forward$/, '');
-            });
-            
-            if (firstNameRegex.test(name) && lastNameRegex.test(name)) {
-                const status = await result.evaluate(ele => {
-                    const statusElement = ele.getElementsByClassName("ps-contact__element")[0];
-                    // Need to remove leading 'Registration status: '
-                    return statusElement.textContent.trim().replace(/Registration status:\s*/, '');
-                })
-                if (status === ScraperBC.activeStatus) {
-                    return true;
-                }
-                else if (ScraperBC.inactiveStatuses.includes(status)) {
-                    return false;
-                }
-                else {
-                    return null;
+            for (const result of results) {
+                const name = await result.evaluate(ele => {
+                    const nameElement = ele.getElementsByTagName("a")[0];
+                    // Need to remove trailing arrow_forward from a connected span element
+                    return nameElement.textContent.trim().replace(/\s*arrow_forward$/, '');
+                });
+                
+                if (firstNameRegex.test(name) && lastNameRegex.test(name)) {
+                    const status = await result.evaluate(ele => {
+                        const statusElement = ele.getElementsByClassName("ps-contact__element")[0];
+                        // Need to remove leading 'Registration status: '
+                        return statusElement.textContent.trim().replace(/Registration status:\s*/, '');
+                    })
+                    if (status === ScraperBC.activeStatus) {
+                        return true;
+                    }
+                    else if (ScraperBC.inactiveStatuses.includes(status)) {
+                        return false;
+                    }
+                    else {
+                        console.warn(`Unknown status: ${status}, for ${prescriber.firstName} ${prescriber.lastName}.`);
+                        return null;
+                    }
                 }
             }
+        } catch (e) {
+            console.error(`Error trying to verify: ${prescriber.firstName} ${prescriber.lastName}. ${e}`);
         }
 
+        console.warn(`Not found in matches: ${prescriber.firstName} ${prescriber.lastName}.`)
         return null;
     }
 }
