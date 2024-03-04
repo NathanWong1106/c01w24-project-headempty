dac5aef264e3b4142189ce6987cae781c721eedb
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2024-03-03 22:27:28 -0500

    [HMTY-36] Database Util Refactor and Scaffold Admin Service (#29)
    * HMTY-36 Database Util Refactor and Scaffold Admin Service
    
    * HMTY-36 Update Init Comment

---
+++ backend/types/adminServiceTypes.js
@@ -0,0 +1,34 @@
+class ClientUserInfo {
+    constructor(email, firstName, lastName, language, city, province, address) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+        this.city = city;
+        this.province = province;
+        this.address = address;
+    }
+}
+
+/**
+ * Currently unused. Used to implement admin patient stuff later.
+ */
+export class PatientInfo extends ClientUserInfo {
+    constructor(email, firstName, lastName, language, city, province, address) {
+        super(email, firstName, lastName, language, city, province, address);
+    }
+}
+
+/**
+ * All necessary information for a single prescriber.
+ */
+export class PrescriberInfo extends ClientUserInfo {
+    constructor(email, firstName, lastName, language, city, province, address, profession, providerCode, licensingCollege, licenseNumber, registered) {
+        super(email, firstName, lastName, language, city, province, address);
+        this.profession = profession;
+        this.providerCode = providerCode;
+        this.licensingCollege = licensingCollege;
+        this.licenseNumber = licenseNumber;
+        this.registered = registered;
+    }
+}
