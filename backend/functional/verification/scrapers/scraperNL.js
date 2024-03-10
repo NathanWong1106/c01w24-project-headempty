81b68fd92da662ce8e938a6bdc014e0f6fd92676
Vidhi Shah <vidhishah@Vidhis-MacBook-Air.local>
2024-03-10 00:05:23 -0500

    Created NL Scraper

--- backend/functional/verification/scrapers/scraperNL.js
+++ backend/functional/verification/scrapers/scraperNL.js
@@ -3,7 +3,6 @@ import { Page } from "puppeteer";
 
 export class ScraperNL extends BaseScraper {
     static scrapeUrl = "https://cpsnl.ca/physician-search/";
-    static firstNameLocator = "#ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_Input2_TextBox1";
     static lastNameLocator = "#ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_Input1_TextBox1";
     static licenceLocator = "#ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_Input0_TextBox1";
     static searchButtonLocator = "#ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_SubmitButton";
@@ -14,58 +13,45 @@ export class ScraperNL extends BaseScraper {
     static invalidStatus = "Non-Practicing"
 
     static async getStatus(prescriber, driver) {
-        
-        // Similar to ScraperPE
         // Uses license number, last name, first name
         try {
-            const firstNameRegex = new RegExp("\\b" + prescriber.firstName + "\\b");
-            const lastNameRegex = new RegExp("\\b" + prescriber.lastName + "\\b");
+
+            //the liscence no format in the csv is diff from the website formatting
+            const licenceNo = prescriber.licenceNumber.slice(0, 1) + " " + prescriber.licenceNumber.slice(1);
 
             await driver.goto(ScraperNL.scrapeUrl, {waitUntil: 'networkidle2'});
             
-            // Original input preserved after leaving site, must clear input first before enter
-            //await driver.waitForSelector(ScraperNL.firstNameLocator);
-            //await driver.$eval(ScraperNL.firstNameLocator, ele => ele.value = "");
             await driver.waitForNetworkIdle();
             await driver.waitForSelector('iframe');
             const elementHandle = await driver.$(
                 'iframe[src="https://imis.cpsnl.ca/WEB/CPSNL/PhysicianSearch/Physician_Search_New.aspx"]',
             );
+
+            //searching on license number and last name only
             const frame = await elementHandle.contentFrame();
-            await frame.$eval(ScraperNL.firstNameLocator, ele => ele.value = "");
-            await frame.type(ScraperNL.firstNameLocator, prescriber.firstName);
-            await frame.$eval(ScraperNL.lastNameLocator, ele => ele.value = "");
             await frame.type(ScraperNL.lastNameLocator, prescriber.lastName);
-            await frame.$eval(ScraperNL.licenceLocator, ele => ele.value = "");
-            await frame.type(ScraperNL.licenceLocator, prescriber.licenceNumber);
+            await frame.type(ScraperNL.licenceLocator, licenceNo);
             
             await frame.click(ScraperNL.searchButtonLocator)
             await driver.waitForNetworkIdle();
             await driver.waitForNetworkIdle();
             
-        
             const resultTable = await frame.$(ScraperNL.resultTableLocator);
-            // console.log(resultTable);
-            const rows = await resultTable.$$("tr");
-            console.log(rows.length);
+            const lastName = await resultTable.evaluate(ele => {
+                return ele.getElementsByTagName("td")[0].textContent.trim();
+            });
 
-            for (const row of rows) {
-                console.log(row);
-      
-                // Check first name that matches
-                const lastName = await row.$eval('td:nth-child(1)', ele => ele.textContent);
-                if (!lastName.match(prescriber.lastName)) {
-                    //do something eror checking
-                } else {
-                    const status = await row.$eval('td:nth-child(5)', ele => ele.textContent);
-                    if (status.includes(ScraperNL.validStatus)) {
-                        return true;
-                    } else if (status.includes(ScraperNL.invalidStatus)) {
-                        return false;
-                    }
-                }
-                    
-            }
+            if (lastName.match(prescriber.lastName)){
+                const status = await resultTable.evaluate(ele => {
+                    return ele.getElementsByTagName("td")[4].textContent.trim();
+                });
+                if (status === ScraperNL.validStatus) {
+                    return true;
+                } else if (status === ScraperNL.invalidStatus) {
+                    return false;
+                } 
+            } 
+        
         } catch (e) {
             console.error(`Error trying to verify: ${prescriber.firstName} ${prescriber.lastName}. ${e}`);
             return null;
