9eca7f73a496cbe89c18c908e6ae912444c03df8
Avik Chakraborty <92612550+AvikChakraborty1@users.noreply.github.com>
2024-03-14 21:26:48 -0400

    Update backend/tests/registration/patientRegistration.test.js
    Co-authored-by: Chun Kang Lu <chunkanglu@gmail.com>

--- backend/tests/registration/patientRegistration.test.js
+++ backend/tests/registration/patientRegistration.test.js
@@ -37,7 +37,6 @@ test("/register/patient - new patient can register", async () => {
 });
 
 test("/register/patient - existing patient can't register", async () => {
-    // Try logging in coordinator
     let res = await fetch(`${SERVER_URL}/user/register/patient`, {
         method: "POST",
         headers: {
